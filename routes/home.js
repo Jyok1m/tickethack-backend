811babd796e5b04008355ea5fad2a67aeda1ff63
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-01-24 14:39:54 +0100

    Get route is done

--- routes/home.js
+++ routes/home.js
@@ -3,16 +3,35 @@ var router = express.Router();
 require("../models/connection");
 const Trains = require("../models/trains");
 const fetch = require("node-fetch");
+const moment = require("moment");
+const { checkBody } = require("../modules/checkBody");
 
 // Route to GET all the trains available depending on users' inputs:
 
 router.get("/", (req, res) => {
   const { departure } = req.body;
   const { arrival } = req.body;
+  const inputDate = req.body.date;
+  const date = moment(
+    new Date(`${inputDate.slice(6)}-${inputDate.slice(3, 5)}-${inputDate.slice(0, 2)}`)
+  ).format("YYYY-MM-DD");
 
-  Trains.find({ departure, arrival }).then((data) => {
-    console.log(data)
-    res.json({data})
+  if (!checkBody(req.body, ["departure", "arrival"])) {
+    res.json({ result: false, error: "Missing the city of departure or arrival" });
+    return;
+  }
+
+  Trains.findOne({ departure, arrival }).then((data) => {
+    if (data === null) {
+      res.json({ result: false, error: "Woops... No train has been found at this date !" });
+    } else {
+      Trains.find({ departure, arrival }).then((data) => {
+        const trainList = data.filter(
+          (trainData) => moment(trainData.date).format("YYYY-MM-DD") === date
+        );
+        res.json({ result: true, trainList });
+      });
+    }
   });
 });
 
