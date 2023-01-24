dddb35c8df77c469e54e87a032830cb18e1ce4a2
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-01-24 19:08:38 +0100

    Beta test commit on backend - perhaps done.

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,6 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-require("../models/connection")
-const fetch = require("node-fetch")
+require("../models/connection");
+const fetch = require("node-fetch");
+const Booking = require("../models/bookings");
+
+router.get("/", (req, res) => {
+  Booking.find().then((allBookings) => {
+    res.json({ allBookings });
+  });
+});
 
 module.exports = router;
